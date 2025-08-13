547ec0bccc89ce3d26d81751d958902854ff54c7
Haider <haiderraza7516@gmail.com>
2025-08-14 00:30:26 +0500

    make temperature form resposnsive for mobile

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -49,7 +49,7 @@ export default function WeatherComponent(){
     </div>
 
    {opentemperature && (
-    <div className='absolute bg-black opacity-80 text-white px-4 py-2 rounded-xl shadow-lg z-50 w-100 right-15 top-25'>
+    <div className='absolute bg-black opacity-80 text-white px-4 py-2 rounded-xl shadow-lg z-50 w-100 sm:right-15 top-25 right-0 '>
   
      <motion.div
        initial={{ opacity: 0, scale: 0.95 }}
